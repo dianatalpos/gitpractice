c86782755b9de6ce9a9b1f1fd6160c1597b382c1
andrea.talpos <diana.talpos4@gmail.com>
2020-02-06 14:16:03 +0200

    initializedDog

--- gitProject/src/Animal.java
+++ gitProject/src/Animal.java
@@ -4,4 +4,8 @@ public class Animal {
         System.out.println("I am an animal!");
         System.out.println("update made in github");
     }
+    public void eat()
+    {
+        System.out.println("I can eat");
+    }
 }
--- gitProject/src/View.java
+++ gitProject/src/View.java
@@ -3,7 +3,10 @@
 public class View {
     public static void main(String args[]){
         System.out.println("Hello!!");
-        Animal a = new Animal();
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        animal.eat();
+        dog.eat();
     }
 
 }
